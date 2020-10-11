eae4629050e48e6ff8d36841f6403461f6ff4230
DeepakDGanachari <deepakdganachari@techmahindra.com>
2020-10-11 20:56:15 +0530

    text added

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+export default class Myapp extends Component<{}> {
+ 
+  constructor() {
+    super();
+    this.state = {
+      isVisible: true,
+    };
+  }
+  Hide_Splash_Screen = () => {
+    this.setState({
+      isVisible: false,
+    });
+  };
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  componentDidMount() {
+    var that = this;
+    setTimeout(function () {
+      that.Hide_Splash_Screen();
+    }, 5000);
+  }
+
+  render() {
+    let Splash_Screen = (
+      <View style={styles.SplashScreen_RootView}>
+        <View style={styles.SplashScreen_ChildView}>
+          <Image
+            source={require("./images/veerabhadra10.jpg")}
+            style={{ width: "100%", height: "100%", resizeMode: "contain" }}
+          />
+          
+        </View>
+      </View>
+    );
+    return (
+      <View style={styles.MainContainer}>
+        <Text style={{ textAlign: "center" }}>
+          {" With joyful hearts\nwe request your presence at the \nEngagement ceremony uniting Suma and Deepak, \n the 11th of Dec at six oclock in the evening\n KH PATIL Marriage Hall \n APMC-Gadag, Reception to follow"}
+        </Text>
+        {this.state.isVisible === true ? Splash_Screen : null}
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
-  container: {
+  MainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Platform.OS === "ios" ? 20 : 0,
+  },
+
+  SplashScreen_RootView: {
+    justifyContent: "center",
+    flex: 1,
+    margin: 10,
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+
+  SplashScreen_ChildView: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00BCD4",
+    flex: 1,
   },
 });
